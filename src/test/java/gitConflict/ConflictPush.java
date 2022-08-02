a4348fc5135f28ff352dc456a0ede332079e4384
Newmmen <108897467+Newmmen@users.noreply.github.com>
2022-08-02 22:19:18 +0300

    first commit

---
+++ src/test/java/gitConflict/ConflictPush.java
@@ -0,0 +1,11 @@
+package gitConflict;
+
+public class ConflictPush {
+    int newNum = 12;
+   void conflictVoid(){
+       if (newNum > 123) {
+           System.out.println(newNum);
+
+       }
+   }
+}
