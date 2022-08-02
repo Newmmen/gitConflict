f25dfd15eb0fff7adc5c8085e870e257cd30a062
Newmmen <108897467+Newmmen@users.noreply.github.com>
2022-08-02 22:25:32 +0300

    edit ConflictPush again

--- src/test/java/gitConflict/ConflictPush.java
+++ src/test/java/gitConflict/ConflictPush.java
@@ -1,7 +1,7 @@
 package gitConflict;
 
 public class ConflictPush {
-    int newNum = 12;
+    int newNum = 1255;
    void conflictVoid(){
        if (newNum > 123) {
            System.out.println(newNum);
